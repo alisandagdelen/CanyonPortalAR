f4a29c435b1fe177cb910d33ce0c8d76a27831e9
Alisan Dagdelen <alisandagdelen@gmail.com>
2018-01-13 20:00:38 +0300

    - Screen setup methods

---
+++ CanyonPortalAR/PortalSceneViewController.swift
@@ -0,0 +1,102 @@
+//
+//  PortalSceneViewController.swift
+//  CanyonPortalAR
+//
+//  Created by alisandagdelen on 13.01.2018.
+//  Copyright © 2018 alisandagdelen. All rights reserved.
+//
+
+import UIKit
+import ARKit
+class PortalSceneViewController: UIViewController, ARSCNViewDelegate {
+    
+    @IBOutlet weak var lblInfo: UILabel!
+    @IBOutlet weak var sceneView: ARSCNView!
+    
+    let configuration = ARWorldTrackingConfiguration()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.sceneView.debugOptions = [ARSCNDebugOptions.showWorldOrigin, ARSCNDebugOptions.showFeaturePoints]
+        self.configuration.planeDetection = .horizontal
+        self.sceneView.session.run(configuration)
+        self.sceneView.delegate = self
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(handleTap))
+        self.sceneView.addGestureRecognizer(tapGestureRecognizer)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // MARK: Screen tap
+    
+    @objc func handleTap(sender: UITapGestureRecognizer) {
+        
+        guard let sceneView = sender.view as? ARSCNView else {return}
+        let touchLocation = sender.location(in: sceneView)
+        let hitTestResult = sceneView.hitTest(touchLocation, types: .existingPlaneUsingExtent)
+        if !hitTestResult.isEmpty {
+            self.lblInfo.isHidden = true
+            self.addPortal(hitTestResult: hitTestResult.first!)
+        }
+    }
+    
+    // MARK: Render
+
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        
+        guard anchor is ARPlaneAnchor else {return}
+        DispatchQueue.main.async {
+            self.lblInfo.isHidden = false
+            self.lblInfo.text = "Plane Detected";
+        }
+        DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+            self.lblInfo.text = "Tap floor to add portal!"
+        }
+    }
+    
+    // MARK: Screen setup methods
+
+    func addPortal(hitTestResult: ARHitTestResult) {
+        
+        let portalScene = SCNScene(named: "Portal.scnassets/Portal.scn")
+        let portalNode = portalScene!.rootNode.childNode(withName: "Portal", recursively: false)!
+        let transform = hitTestResult.worldTransform
+        let planeXposition = transform.columns.3.x
+        let planeYposition = transform.columns.3.y
+        let planeZposition = transform.columns.3.z
+        portalNode.position =  SCNVector3(planeXposition, planeYposition, planeZposition)
+        self.sceneView.scene.rootNode.addChildNode(portalNode)
+        self.addPlane(nodeName: "roof", portalNode: portalNode, imageName: "top")
+        self.addPlane(nodeName: "floor", portalNode: portalNode, imageName: "bottom")
+        self.addWalls(nodeName: "backWall", portalNode: portalNode, imageName: "sideTwo")
+        self.addWalls(nodeName: "sideWallA", portalNode: portalNode, imageName: "sideThree")
+        self.addWalls(nodeName: "sideWallB", portalNode: portalNode, imageName: "sideOne")
+        self.addWalls(nodeName: "sideDoorA", portalNode: portalNode, imageName: "sideFourFirstHalf")
+        self.addWalls(nodeName: "sideDoorB", portalNode: portalNode, imageName: "sideFourSecondHalf")
+    }
+    
+    func addWalls(nodeName: String, portalNode: SCNNode, imageName: String) {
+       
+        let child = portalNode.childNode(withName: nodeName, recursively: true)
+        child?.geometry?.firstMaterial?.diffuse.contents = UIImage(named: "Portal.scnassets/\(imageName).png")
+        child?.renderingOrder = 200
+        if let mask = child?.childNode(withName: "mask", recursively: false) {
+            mask.geometry?.firstMaterial?.transparency = 0.000001
+        }
+    }
+    
+    
+    func addPlane(nodeName: String, portalNode: SCNNode, imageName: String) {
+       
+        let child = portalNode.childNode(withName: nodeName, recursively: true)
+        child?.geometry?.firstMaterial?.diffuse.contents = UIImage(named: "Portal.scnassets/\(imageName).png")
+        child?.renderingOrder = 200
+    }
+    
+}
+
+
